20b385fae7effc8c222e86c95a8e3946ba26c64d
Toby Retallick <toby.retallick@gmail.com>
2014-11-22 00:38:34 +0700

    Cells die when population is one or two

--- src/main/java/com/codurance/World.java
+++ src/main/java/com/codurance/World.java
@@ -1,13 +1,24 @@
 package com.codurance;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class World {
 
-    private List<Cell> population = new ArrayList<>();
+    private List<Cell> population;
+
+    public World(List<Cell> initialPopulation) {
+        this.population = initialPopulation;
+    }
 
     public List<Cell> getPopulation() {
         return population;
+    }
+
+    public void tick() {
+        if(population.size() == 1 || population.size() == 2) {
+            for(Cell cell : population) {
+                cell.dead();
+            }
+        }
     }
 }
--- src/test/java/com/codurance/WorldShould.java
+++ src/test/java/com/codurance/WorldShould.java
@@ -2,26 +2,61 @@ package com.codurance;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class WorldShould {
 
     private World world;
+    @Mock Cell cell;
+    private List<Cell> initialPopulation;
 
     @Before
     public void initialise() {
-        world = new World();
     }
-    
+
     @Test public void
     should_be_empty_when_initialised() {
+        List<Cell> initialPopulation = new ArrayList<>();
+        world = new World(initialPopulation);
         assertThat(world.getPopulation().size(), is(0));
     }
 
     @Test public void
-    should_be_empty_when_regenerated_if_there_is_only_one_live_cell() {
-        assertThat(, is());
+    kill_cell_if_it_does_not_have_any_neighbours() {
+        createCells(1);
+        world = new World(initialPopulation);
+        assertThat(world.getPopulation().size(), is(1));
+        world.tick();
+        verify(cell).dead();
     }
+
+    @Test public void
+    kill_cells_if_population_size_is_2() {
+        createCells(2);
+        world = new World(initialPopulation);
+        assertThat(world.getPopulation().size(), is(2));
+        world.tick();
+        verify(cell,times(2)).dead();
+    }
+
+    private void createCells(int number) {
+
+        initialPopulation = new ArrayList<>();
+        for(int i=0; i<number; i++) {
+            initialPopulation.add(cell);
+        }
+    }
+
+
 }
