8849647ba2f131f49d932199bfd15104ffe09df3
Toby Retallick <toby.retallick@gmail.com>
2014-11-27 09:34:22 +0000

    Reset - starting again

--- src/main/java/com/codurance/World.java
+++ src/main/java/com/codurance/World.java
@@ -1,21 +1,5 @@
 package com.codurance;
 
-import java.util.List;
-
 public class World {
-    private final Grid grid;
-    private List<Cell> population;
 
-    public World(List<Cell> initialPopulation, Grid grid) {
-        this.population = initialPopulation;
-        this.grid = grid;
-    }
-
-    public void tick() {
-        population = grid.returnNextGeneration(population);
-    }
-
-    public List<Cell> getPopulation() {
-        return population;
-    }
 }
--- src/test/java/com/codurance/WorldShould.java
+++ src/test/java/com/codurance/WorldShould.java
@@ -1,38 +1,10 @@
 package com.codurance;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WorldShould {
 
-    private World world;
-    @Mock Grid grid;
-    @Mock Cell cell;
-    @Mock LivingCells livingCells;
 
-    @Test public void
-    should_have_a_population_of_zero_if_initialised_with_one_living_cell() {
-        List<Cell> livingCells = new ArrayList();
-        livingCells.add(cell);
-
-        List<Cell> nextGeneration = new ArrayList();
-
-        world  = new World(livingCells, grid);
-
-        given(grid.returnNextGeneration(livingCells)).willReturn(nextGeneration);
-
-        world.tick();
-
-        assertThat(world.getPopulation().isEmpty(), is(true));
-    }
 }
