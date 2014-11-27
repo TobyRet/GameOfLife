a939caa0cd87330078ffd1a26ee42bd7b0af850f
Toby Retallick <toby.retallick@gmail.com>
2014-11-27 09:45:00 +0000

    World is empty when created

--- src/main/java/com/codurance/World.java
+++ src/main/java/com/codurance/World.java
@@ -1,5 +1,17 @@
 package com.codurance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
 
+    private List<Cell> population = new ArrayList<>();
+
+    public static World empty() {
+        return new World();
+    }
+
+    public boolean isEmpty() {
+        return population.size() == 0;
+    }
 }
--- src/test/java/com/codurance/WorldShould.java
+++ src/test/java/com/codurance/WorldShould.java
@@ -1,10 +1,19 @@
 package com.codurance;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WorldShould {
 
+    @Test public void
+    world_should_be_empty_when_created() {
+        World world = World.empty();
+        assertThat(world.isEmpty(), is(true));
+    }
 
 }
